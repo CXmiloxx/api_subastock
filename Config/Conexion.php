46a7febc0576c59b20d2929d478564c1e977f43b
Camilo <137653825+CXmiloxx@users.noreply.github.com>
2024-08-19 14:18:13 -0500

    base de datos desplegada camios en estructura

--- Config/Conexion.php
+++ Config/Conexion.php
@@ -3,11 +3,17 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type");
+require 'vendor/autoload.php';
 
-$servidor = "localhost";
-$usuario = "root";
-$contrasena = '';
-$nombre_de_base = 'subastock';
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$servidor = $_ENV['MYSQL_ADDON_HOST'];
+$usuario = $_ENV['MYSQL_ADDON_USER'];
+$contrasena = $_ENV['MYSQL_ADDON_PASSWORD'];
+$nombre_de_base = $_ENV['MYSQL_ADDON_DB'];
 
 try {
     $base_de_datos = new PDO("mysql:host=$servidor; dbname=$nombre_de_base", $usuario, $contrasena);
